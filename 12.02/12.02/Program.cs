5b9ea6f45db01a53b008292eee7a84d257477696
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-02-16 13:43:08 +0200

    Add files via upload

---
+++ 12.02/12.02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolko dushi shte vivejdash?: ");
+            int hora = int.Parse(Console.ReadLine());
+            string[] ime = new string[hora];
+            double[] djobni = new double[hora];
+
+            Console.WriteLine("Vivejdane na danni: ");
+            for (int i = 0; i < hora; i++)
+            {
+                Console.WriteLine("ime");
+                ime[i] = Console.ReadLine();
+                Console.WriteLine("djobni");
+                djobni[i] = int.Parse(Console.ReadLine()); Console.WriteLine("ime");
+            }
+            Console.WriteLine("Izhod 1: ");
+            for (int i = 0; i < hora; i++)
+            {
+                Console.WriteLine(ime[hora] + "ima" + djobni[hora] + "lv");
+            }
+            Console.WriteLine("Izhod 2: ");
+            if (djobni &> "7")
+            {
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Izhod 3: ");
+            if (hora==0)
+            {
+                int banica =-2;
+                Console.WriteLine(ime[hora] + banica);
+            }
+        }
+    }
+}
---
+++ 14.02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolko dushi shte vivejdash?; ");
+            int n=int.Parse(Console.ReadLine());
+            string[] ime = new string[n];
+            string[] items;
+
+
+            for (int i = 0; i < n;i++)
+            {
+                Console.Write("Vivedi trite imena; ");
+                ime[i] = Console.ReadLine();
+                items = ime[i].Split(' ').ToArray();
+               // string[] ime = Console.ReadLine().Split(' ').ToArray();
+                Console.WriteLine("ime[{0}] = {1}", i, items[i]);
+                Console.WriteLine("ime[{0}] = {1}", i + 1, items[i+1]);
+                Console.WriteLine("ime[{0}] = {1}", i + 2, items[i+2]);
+            }
+            //Izhod 2: Izvejdane na elementi ot posledniq do pirviq
+            for (int i = n - 1; i > 0; i--)
+            { 
+                
+                items = ime[i].Split(' ').ToArray();
+                Console.WriteLine("ime[{0}] = {1}", i + 2, items[i + 2]);
+                Console.WriteLine("ime[{0}] = {1}", i + 1, ime[i + 1]);
+                Console.WriteLine("ime[{0}] = {1}", i, items[i]);
+            }
+            //Izhod 3:
+            for (int i = 0; i < n; i++)
+            {
+               
+                items = ime[i].Split(' ').ToArray();
+                Console.WriteLine("ime[{0}] = {1}", i, items[i]);
+                Console.WriteLine("ime[{0}] = {1}", i + 2, items[i + 2]);
+            }
+        }
+    }
+}

aa0f3341d01868b5c1cd8d0be1e8e8aab5ca43f8
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-02-21 08:06:10 +0200

    Add files via upload

---
+++ 19.02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cikil s 5 elementa,
+          
+            int[] nomer = new int[5];
+            string[] ime = new string[5];
+            //vhod
+            for (int i= 0; i<5 ; i++)
+            { Console.WriteLine("Nomer; ");
+               nomer[i]=int.Parse(Console.ReadLine());
+                Console.WriteLine("Ime; ");
+                ime[i]= Console.ReadLine();
+
+            }
+            //izhod
+            for (int i=0; i<5; i++)
+                
+            {
+                Console.WriteLine(nomer[i] + "/" + ime[i]);
+            }
+            //Izhod 2; Da iskara vsicki nomera nad 3
+            for (int i = 0; i <5; i++)
+            {
+                if (nomer[i]>3)
+                Console.WriteLine(nomer[i] + "/" + ime[i]);
+            }
+            //izhod 3
+            Console.Write("Vivedi kakvo ime da tirsq;");
+            string novoime=Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (ime[i] == novoime)
+                {
+                    Console.WriteLine(nomer[i] + ime[i]);
+                }
+                else
+                {
+                    Console.Write("Error");
+                }
+            }
+        }
+    }
+}
---
+++ Zad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadelqne na pamet
+            double[] pari = new double[2];
+            string[] vid_Zakuska = new string[2];
+            //Vhod
+            for (int i = 0;i < vid_Zakuska.Length;i++) 
+            {
+                Console.WriteLine("Vivedi zakuska;");
+                vid_Zakuska[i]= Console.ReadLine();
+                Console.WriteLine("Vivedi pari");
+                pari[2]=double.Parse(Console.ReadLine()) ;
+            }
+            //Izhod
+            for (int i = 0; i <2; i++)
+            {
+                int b=int.Parse(Console.ReadLine());
+                Console.Write(vid_Zakuska[i] +"/"+ pari[i]);
+            }
+        }
+    }
+}
